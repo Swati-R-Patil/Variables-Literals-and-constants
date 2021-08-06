8a731ffe0161163db53657a42f8a1f39640be85c
Pravin Kunnure <pravinkunnure9@gmail.com>
2021-08-06 18:32:22 +0530

    Variables,Literal and Constants

---
+++ src/com/example/Variables_Literals_Constants.java
@@ -0,0 +1,40 @@
+package com.example;
+
+public class Variables_Literals_Constants
+{
+    public static void main(String[] args)
+    {
+        int firstNumber;
+        int secondNumber;
+
+        firstNumber=11;
+        secondNumber=20;
+
+        double average;
+        average=(firstNumber+secondNumber)/2.0;
+
+        System.out.println("Average: "+average);
+
+        String name;
+        String surname;
+
+
+        name="John";
+        surname="Rambo";
+        System.out.println("$surname");
+
+      //  System.out.println(name+" "+surname);
+
+        boolean value;
+        value=false;
+
+        float a=3.14f;
+
+        System.out.println(value);
+
+        char letter;
+        letter='j';
+
+        System.out.println(letter);
+    }
+}
